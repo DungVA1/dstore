d83aed238cd406275792bc7fefd7618745df2ee9
DungVA <dungva1505@gmail.com>
2025-10-11 11:23:51 +0700

    user repo

--- src/apps/user-service/application/user-repository.interface.ts
+++ src/apps/user-service/application/user-repository.interface.ts
@@ -2,8 +2,8 @@ import { UserModel } from '../infrastructure/user.orm';
 
 export interface IUserRepository {
   save(userModel: UserModel): Promise<UserModel>;
-  getById(id: string): Promise<UserModel>;
-  getList(options): Promise<[UserModel]>;
+  getById(id: string): Promise<UserModel | null>;
+  getList(options): Promise<UserModel[]>;
   delete(id: string): Promise<void>;
   updated(id: string, userModel: UserModel): Promise<UserModel>;
 }
