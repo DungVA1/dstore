c53e6a08caeca0d90e43d87d18cfb9e3b83b0103
DungVA <dungva1505@gmail.com>
2025-11-11 12:33:32 +0700

    fix

--- src/libs/shared/generator/generator.module.ts
+++ src/libs/shared/generator/generator.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { GeneratorService } from './generator.service';
 
 @Module({
+  imports: [ConfigModule],
   providers: [GeneratorService],
   exports: [GeneratorService],
 })
--- src/libs/shared/generator/generator.service.ts
+++ src/libs/shared/generator/generator.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { authenticator } from 'otplib';
 import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class GeneratorService {
+  constructor(private readonly configService: ConfigService) {}
   generateId(): string {
     return uuidv4();
   }
 
   generateOTP(): string {
-    const token = authenticator.generate(process.env.JWT_SECRET as string);
+    const token = authenticator.generate(
+      this.configService.get('secret.otp') as string,
+    );
 
     return token;
   }
